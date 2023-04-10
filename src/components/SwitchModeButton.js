19ec62b43df723aa0f019b2b5d23e4718a0320d2
Ido Bouskila <idobouskila22@gmail.com>
2023-04-11 00:02:06 +0300

    fix: Control default switch value from state

--- src/components/SwitchModeButton.js
+++ src/components/SwitchModeButton.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import * as Switch from '@radix-ui/react-switch';
-import { toggleNoteMode } from '../redux/notesSettings';
-import { useDispatch } from 'react-redux';
+import { selectIsNotMode, toggleNoteMode } from '../redux/notesSettings';
+import { useDispatch, useSelector } from 'react-redux';
 
 const SwitchModeButton = () => {
     const dispatch = useDispatch();
-
+    const isNoteMode = useSelector(selectIsNotMode);
+    
     return (
         <header>
             <label htmlFor='note-mode'>Note Mode</label>
 
             <Switch.Root
+                checked={ isNoteMode }
                 onClick={ () => dispatch(toggleNoteMode()) }
                 className='toggle'
                 id='note-mode'
